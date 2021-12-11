b9bcdfd538338d6e5f8c9c6b167a17eb0a7f70bc
Xiao Xia <xiaoxia.51@outlook.com>
2021-12-11 13:37:03 -0800

    test gshare predictor

--- src/test.sh
+++ src/test.sh
@@ -1,7 +1,13 @@
 make
+echo "PREDICT fp_1:"
 bunzip2 -kc ../traces/fp_1.bz2 | ./predictor --$1
+echo "PREDICT fp_2:"
 bunzip2 -kc ../traces/fp_2.bz2 | ./predictor --$1
+echo "PREDICT int_1:"
 bunzip2 -kc ../traces/int_1.bz2 | ./predictor --$1
+echo "PREDICT int_2:"
 bunzip2 -kc ../traces/int_2.bz2 | ./predictor --$1
+echo "PREDICT mm_1:"
 bunzip2 -kc ../traces/mm_1.bz2 | ./predictor --$1
+echo "PREDICT mm_2:"
 bunzip2 -kc ../traces/mm_2.bz2 | ./predictor --$1
